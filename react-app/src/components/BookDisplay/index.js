e9fbd62ed9c03a7d3af2a3b239f3afd0a0be5f90
Emily Morgan <mocemmy@aol.com>
2023-08-11 19:53:26 -0400

    styled user home page

--- react-app/src/components/BookDisplay/index.js
+++ react-app/src/components/BookDisplay/index.js
@@ -1,33 +1,50 @@
 import { useDispatch } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
 import { thunkDeleteBook } from "../../store/book";
+import OpenModalButton from "../OpenModalButton";
+import ConfirmModal from "../ConfirmModal";
+import "./BookDisplay.css";
 
 function BookDisplay({ book, type }) {
   const history = useHistory();
   const dispatch = useDispatch();
 
   const handleEditBook = () => {
-    history.push(`/app/books/${book.id}/edit`)
-  }
+    history.push(`/app/books/${book.id}/edit`);
+  };
 
   const handleDeleteBook = () => {
-    dispatch(thunkDeleteBook(book.id))
-  }
+    dispatch(thunkDeleteBook(book.id));
+  };
 
   return (
     <div className="book-container">
-    <Link to={`/app/books/${book.id}/details`} >
-      <img src={book.book_cover} alt="book cover" />
+      <Link to={`/app/books/${book.id}/details`}>
+        <img id="book-display-image" src={book.book_cover} alt="book cover" />
       </Link>
       <div>
         <Link to={`/app/books/${book.id}/details`}>{book.title}</Link>
         <p>
           {book.author_first_name}&nbsp;{book.author_last_name}
         </p>
-        {type === "OWNED" && <button onClick={handleEditBook}>Edit book</button>}
-        {type === "OWNED" && <button onClick={handleDeleteBook}>Delete book</button>}
+        <div className="options-container">
+          {type === "OWNED" && (
+            <button onClick={handleEditBook}>Edit book</button>
+          )}
+          {type === "OWNED" && (
+            <OpenModalButton
+              className="button-as-link"
+              buttonText={"Delete book"}
+              modalComponent={
+                <ConfirmModal
+                  modalTitle="Are you sure you want to delete your book?"
+                  yesHandler={handleDeleteBook}
+                />
+              }
+            />
+          )}
+        </div>
       </div>
-    
     </div>
   );
 }
