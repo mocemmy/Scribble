bd3dab81377581ceae8ac0f8c87d90c61332410c
Emily Morgan <mocemmy@aol.com>
2023-08-11 12:21:21 -0400

    finished basic review display styling

--- react-app/src/components/ReviewDisplay/index.js
+++ react-app/src/components/ReviewDisplay/index.js
@@ -1,7 +1,30 @@
-function ReviewDisplay ({ review }) {
-    return (
-        <h1>{review.review_body}</h1>
-    )
+import './ReviewDisplay.css';
+
+function ReviewDisplay({ review }) {
+    //format date as month dd, yyyy
+  const time = new Date(review.created_at);
+  const dispTime = time.toLocaleDateString('en-US', {
+    month: "long",
+    day: "numeric",
+    year: "numeric"
+  });
+
+  return (
+    <div className="review-container">
+      <div className="user-info">
+        <img src={review.user.profile_pic} alt="profile pic" />
+        {review.user.first_name}
+        <p>follow</p>
+      </div>
+      <div className="review-content">
+        <div className="review-header">
+          <p>{review.review_stars}</p>
+          <p>{dispTime}</p>
+        </div>
+        <p>{review.review_body}</p>
+      </div>
+    </div>
+  );
 }
 
 export default ReviewDisplay;
