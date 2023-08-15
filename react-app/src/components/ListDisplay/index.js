b067754b9ffd872733627e82a2b2f72844661139
Emily Morgan <mocemmy@aol.com>
2023-08-15 17:20:05 -0400

    styled browse lists

--- react-app/src/components/ListDisplay/index.js
+++ react-app/src/components/ListDisplay/index.js
@@ -7,15 +7,15 @@ function ListDisplay({ list }) {
         bookList = bookList.slice(0,5)
     }
     return(
-        <>
+        <div className='list-item-container'>
         <Link to={`/app/lists/${list.id}/details`} className='list-display-container'>
             {bookList.map(book => (
                 <img className="list-book-covers" src={book.book_cover} alt='cover' id={book.id}/>
             ))}
-            <p>{list.list_name}</p>
+            <p id='list-title'>{list.list_name}</p>
         </Link>
         {list.books.length > 1 ? <p className='list-length'>{list.books.length} books</p> : <p className='list-length'>{list.books.length} book</p>}
-        </>
+        </div>
     )
 }
 
