bfbc241c9691877a8e3522de9ec9386f08a23480
Emily Morgan <mocemmy@aol.com>
2023-08-15 17:52:31 -0400

    fixed sticky position of book covers on details page

--- react-app/src/components/BookDetails/index.js
+++ react-app/src/components/BookDetails/index.js
@@ -12,35 +12,36 @@ function BookDetails() {
   const book = useSelector((state) => state.books.SingleBook);
   const dispatch = useDispatch();
 
-
   useEffect(() => {
     dispatch(thunkGetBookDetails(bookId));
   }, [dispatch, bookId]);
 
   if (!book || +bookId !== book.id) return <Loading />;
   return (
-    <>
-      <div className="book-details-page-container">
-        <div className="book-details-page">
-          <div className="book-image-container">
-            <img id="book-cover" src={book.book_cover} alt="book cover" />
-            <p onClick={e=> window.alert("Feature coming soon")}>Want to read</p>
-          </div>
-          <div className="book-info-container">
-            <div>
-              <h1>{book.title}</h1>
-              <h3>
-                {book.author_first_name}&nbsp;{book.author_last_name}
-              </h3>
-              <ReviewInfoDisplay book={book}/>
-              <p>{book.summary}</p>
+    <div className="book-details-page-container">
+      <div className="book-image-container">
+        <img id="book-cover" src={book.book_cover} alt="book cover" />
+        <p onClick={(e) => window.alert("Feature coming soon")}>Want to read</p>
+      </div>
+      <div className="right-side-container">
+        <div className="book-info-container">
+          <div className="book-details-page">
+            <div className="book-info-container">
+              <div>
+                <h1>{book.title}</h1>
+                <h3>
+                  {book.author_first_name}&nbsp;{book.author_last_name}
+                </h3>
+                <ReviewInfoDisplay book={book} />
+                <p>{book.summary}</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <ReviewSummary bookId={book.id} />
-    </>
+        <ReviewSummary bookId={book.id} />
+      </div>
+    </div>
   );
 }
 
