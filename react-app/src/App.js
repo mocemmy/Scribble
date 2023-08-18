b48c2c60abd988bc4cbc452282e9cf5c4ec96fcb
Emily Morgan <mocemmy@aol.com>
2023-08-18 14:41:04 -0400

    styled landing page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,24 +2,23 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import { authenticate } from "./store/session";
-import LandingPage from './components/SplashPageComponents/LandingPage'
-import NavBar from './components/NavBar'
-import BrowseBooks from './components/BookComponents/BrowseBooks'
+import LandingPage from "./components/SplashPageComponents/LandingPage";
+import NavBar from "./components/NavBar";
+import BrowseBooks from "./components/BookComponents/BrowseBooks";
 import BrowseLists from "./components/ListComponents/BrowseLists";
-import BookDetails from './components/BookComponents/BookDetails'
-import EditBook from './components/BookComponents/EditBook'
-import ReviewForm from './components/ReviewComponents/ReviewForm'
-import SearchResults from './components/Search/SearchResults'
-import UserHomePage from './components/UserHomePage'
+import BookDetails from "./components/BookComponents/BookDetails";
+import EditBook from "./components/BookComponents/EditBook";
+import ReviewForm from "./components/ReviewComponents/ReviewForm";
+import SearchResults from "./components/Search/SearchResults";
+import UserHomePage from "./components/UserHomePage";
 import BookForm from "./components/BookComponents/BookForm";
-import ListForm from './components/ListComponents/ListForm'
-import ListDetails from './components/ListComponents/ListDetails'
-import BookshelfDetails from './components/BookshelfComponents/BookshelfDetails'
-import AppHomePage from './components/AppHomePage'
-import LoginFormPage from './components/AuthComponents/LoginFormPage'
-import SignupFormPage from './components/AuthComponents/SignupFormPage'
+import ListForm from "./components/ListComponents/ListForm";
+import ListDetails from "./components/ListComponents/ListDetails";
+import BookshelfDetails from "./components/BookshelfComponents/BookshelfDetails";
+import AppHomePage from "./components/AppHomePage";
+import LoginFormPage from "./components/AuthComponents/LoginFormPage";
+import SignupFormPage from "./components/AuthComponents/SignupFormPage";
 import MyBooksPage from "./components/BookshelfComponents/MyBooksPage";
-
 
 function App() {
   const dispatch = useDispatch();
@@ -37,53 +36,53 @@ function App() {
           </Route>
           <Route path="/app">
             <NavBar />
-            <Switch>
-              <Route path="/app/browse-books">
-                <BrowseBooks />
-              </Route>
-              <Route path="/app/my-books">
-                <MyBooksPage />
-              </Route>
-              <Route path='/app/browse-lists'>
-                <BrowseLists />
-              </Route>
-              <Route path="/app/books/:bookId/details">
-                <BookDetails />
-              </Route>
-              <Route path="/app/books/:bookId/edit">
-                <EditBook type="EDIT" />
-              </Route>
-              <Route path='/app/books/:bookId/review'>
-                <ReviewForm type="CREATE" />
-              </Route>
-              <Route path='/app/reviews/:reviewId/edit'>
-                <ReviewForm type="EDIT" />
-              </Route>
-              <Route path='/app/books/search'>
-                <SearchResults />
-              </Route>
-              <Route path='/app/user'>
-                <UserHomePage />
-              </Route>
-              <Route path="/app/create-book">
-                <BookForm type='CREATE'/>
-              </Route>
-              <Route path='/app/create-list'>
-                <ListForm type='CREATE' />
-              </Route>
-              <Route path="/app/lists/:listId/edit">
-                <ListForm type="EDIT" />
-              </Route>
-              <Route path='/app/lists/:listId/details'>
-                <ListDetails />
-              </Route>
-              <Route path='/app/bookshelves/:shelfId/details'>
-                <BookshelfDetails />
-              </Route>
-              <Route path="*">
-                <AppHomePage isLoaded={isLoaded} />
-              </Route>
-            </Switch>
+              <Switch>
+                <Route path="/app/browse-books">
+                  <BrowseBooks />
+                </Route>
+                <Route path="/app/my-books">
+                  <MyBooksPage />
+                </Route>
+                <Route path="/app/browse-lists">
+                  <BrowseLists />
+                </Route>
+                <Route path="/app/books/:bookId/details">
+                  <BookDetails />
+                </Route>
+                <Route path="/app/books/:bookId/edit">
+                  <EditBook type="EDIT" />
+                </Route>
+                <Route path="/app/books/:bookId/review">
+                  <ReviewForm type="CREATE" />
+                </Route>
+                <Route path="/app/reviews/:reviewId/edit">
+                  <ReviewForm type="EDIT" />
+                </Route>
+                <Route path="/app/books/search">
+                  <SearchResults />
+                </Route>
+                <Route path="/app/user">
+                  <UserHomePage />
+                </Route>
+                <Route path="/app/create-book">
+                  <BookForm type="CREATE" />
+                </Route>
+                <Route path="/app/create-list">
+                  <ListForm type="CREATE" />
+                </Route>
+                <Route path="/app/lists/:listId/edit">
+                  <ListForm type="EDIT" />
+                </Route>
+                <Route path="/app/lists/:listId/details">
+                  <ListDetails />
+                </Route>
+                <Route path="/app/bookshelves/:shelfId/details">
+                  <BookshelfDetails />
+                </Route>
+                <Route path="*">
+                  <AppHomePage isLoaded={isLoaded} />
+                </Route>
+              </Switch>
           </Route>
           <Route path="/login">
             <LoginFormPage />
--- react-app/src/components/SplashPageComponents/LandingPage/index.js
+++ react-app/src/components/SplashPageComponents/LandingPage/index.js
@@ -39,29 +39,70 @@ function LandingPage({ isLoaded }) {
           </p>
         </div>
       </div>
-      <div className="discovery-container">
-        <h3>What will you discover?</h3>
-        <div className="book-container">
-          <div>
-            <p>Because Brian liked:</p>
-            <div className="book-container">
-              <img src="/images/default-book-cover.jpg" alt="book" />
-              <img src="/images/default-book-cover.jpg" alt="book" />
-              <img src="/images/default-book-cover.jpg" alt="book" />
-              <img src="/images/default-book-cover.jpg" alt="book" />
+      <div className="body-container">
+        <div className="discovery-container">
+          <h3>What will you discover?</h3>
+          <div className="book-main-container">
+            <div>
+              <p>Because Brian liked:</p>
+              <div className="book-container">
+                <img src="/images/catching-fire-cover.jpg" alt="book" />
+                <img src="/images/mockingjay-cover.jpg" alt="book" />
+                <img src="/images/the-lightning-thief-cover.jpg" alt="book" />
+                <img src="/images/the-titans-curse-cover.jpg" alt="book" />
+                <i className="fa-solid fa-arrow-right-long"></i>
+              </div>
+            </div>
+            <div>
+              <p>He discovered:</p>
+              <div className="book-container">
+                <img src="/images/a-deadly-education-cover.jpg" alt="book" />
+              </div>
             </div>
           </div>
-          <div>
-            <p>He discovered:</p>
-            <img src="/images/default-book-cover.jpg" alt="book" />
+          <div className="book-main-container">
+            <div>
+              <p>Because Laura liked:</p>
+              <div className="book-container">
+                <img src="/images/a-deadly-education-cover.jpg" alt="book" />
+                <img src="/images/the-hunger-games-cover.jpg" alt="book" />
+                <img src="/images/catching-fire-cover.jpg" alt="book" />
+                <img src="/images/pride-and-prejudice-cover.jpg" alt="book" />
+                <i className="fa-solid fa-arrow-right-long"></i>
+              </div>
+            </div>
+            <div>
+              <p>She discovered:</p>
+              <div className="book-container">
+                <img src="/images/emma-cover.jpg" alt="book" />
+              </div>
+            </div>
           </div>
         </div>
+        <div className="quote-container">
+          <h3>Quotes:</h3>
+          <p className="quote">
+            “From that time on, the world was hers for the reading. She would
+            never be lonely again, never miss the lack of intimate friends.
+            Books became her friends and there was one for every mood.”
+            <span>- Betty Smith, A Tree Grows in Brooklyn</span>
+          </p>
+          <p className="quote">
+          “All human wisdom is summed up in these two words – ‘Wait and hope.’”
+            <span>- Alexandre Dumas, The Count of Monte Cristo</span>
+          </p>
+          <p className="quote">
+          “We were the people who were not in the papers. We lived in the blank white spaces at the edges of print. It gave us more freedom. We lived in the gaps between the stories.”
+            <span>- Margaret Atwood, The Handmaid's Tale</span>
+          </p>
+        </div>
       </div>
-
       <div className="footer-container">
         <div>
           <h3>Creator</h3>
-          <Link to="/">About us</Link>
+          <Link onClick={(e) => window.alert("Feature coming soon!")} to="/">
+            About us
+          </Link>
         </div>
         <div>
           <h3>Connect</h3>
