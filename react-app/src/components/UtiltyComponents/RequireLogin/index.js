6d570f3bc42fbf1bc1c5bf3aaf2a0ae70cbe3b8d
Emily Morgan <mocemmy@aol.com>
2023-08-18 17:03:16 -0400

    added alert to redirect

--- react-app/src/components/UtiltyComponents/RequireLogin/index.js
+++ react-app/src/components/UtiltyComponents/RequireLogin/index.js
@@ -5,6 +5,7 @@ function RequireLogin ({isLoaded}) {
     const user = useSelector(state => state.session.user)
     const history = useHistory()
     if(isLoaded && !user){
+        window.alert("You must be logged in to view this page")
         history.push('/')
     }
 
