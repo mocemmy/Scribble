ce95965b1be931d7f125828877e33313a9f38c5d
Emily Morgan <mocemmy@aol.com>
2023-09-05 15:44:42 -0400

    implemented react-pagination component

--- react-app/src/components/BookComponents/BrowseBooks/index.js
+++ react-app/src/components/BookComponents/BrowseBooks/index.js
@@ -1,29 +1,63 @@
-import { useDispatch, useSelector } from 'react-redux'
-import Loading from '../../UtiltyComponents/Loading';
-import { useEffect } from 'react';
-import { thunkGetAllBooks } from '../../../store/book';
-import BookDisplay from '../../BookComponents/BookDisplay';
-import { thunkGetBookshelvesCurr } from '../../../store/bookshelf';
+import { useDispatch, useSelector } from "react-redux";
+import Loading from "../../UtiltyComponents/Loading";
+import { useEffect, useState } from "react";
+import { thunkGetAllBooks } from "../../../store/book";
+import BookDisplay from "../../BookComponents/BookDisplay";
+import { thunkGetBookshelvesCurr } from "../../../store/bookshelf";
+import ReactPaginate from "react-paginate";
+import './BrowseBooks.css'
+
 
 function BrowseBooks() {
-    const books = useSelector(state => state.books.AllBooks)
-    const dispatch = useDispatch()
+  const books = useSelector((state) => state.books.AllBooks);
+  const [currPage, setCurrPage] = useState(1);
+  const [totalPages, setTotalPages] = useState();
+  const booksPerPage = 20;
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(thunkGetAllBooks());
-        dispatch(thunkGetBookshelvesCurr())
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(thunkGetAllBooks());
+    dispatch(thunkGetBookshelvesCurr());
+  }, [dispatch]);
 
-    if(!books) return <Loading />
-    const booksArr = Object.values(books);
-    return (
-        <div>
-            {!!booksArr.length && booksArr.map(book => (
-                <BookDisplay key={book.id} book={book} />
-            ))}
-            {!booksArr.length && <h4>No books to browse!</h4>}
-        </div>
-    )
+  useEffect(() => {
+    if (books) {
+      const booksArr = Object.values(books).reverse();
+      setTotalPages(Math.ceil(booksArr.length / booksPerPage));
+    }
+  }, [books]);
+  if (!books) return <Loading />;
+  const booksArr = Object.values(books).reverse();
+
+  const handlePageChange = (selectedPage) => {
+    setCurrPage(selectedPage.selected);
+  };
+
+  const startIndex = currPage * booksPerPage;
+  const endIndex = startIndex + booksPerPage;
+  const subset = booksArr.slice(startIndex, endIndex);
+
+  return (
+    <div>
+      <div className="pagination-container">
+        <ReactPaginate
+          pageCount={totalPages}
+          onPageChange={handlePageChange}
+          forcePage={currPage}
+        />
+      </div>
+      {!!subset.length &&
+        subset.map((book) => <BookDisplay key={book.id} book={book} />)}
+      {!subset.length && <h4>No books to browse!</h4>}
+      <div className="pagination-container">
+        <ReactPaginate
+          pageCount={totalPages}
+          onPageChange={handlePageChange}
+          forcePage={currPage}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default BrowseBooks;
