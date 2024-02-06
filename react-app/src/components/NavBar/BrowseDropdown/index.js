705cf5ba674a551772d475aabcf04efa7b90911d
Emily <mocemmy@aol.com>
2024-02-05 20:05:46 -0500

    fixed browse books dropdown to be separate

--- react-app/src/components/NavBar/BrowseDropdown/index.js
+++ react-app/src/components/NavBar/BrowseDropdown/index.js
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom'
 import './BrowseDropdown.css'
 
 
-function BrowseDropdown () {
+function BrowseDropdown() {
     const [showMenu, setShowMenu] = useState(false)
     const dropdownRef = useRef();
 
     useEffect(() => {
-        if(!showMenu) return;
+        if (!showMenu) return;
 
         const closeMenu = (e) => {
-            if(dropdownRef.current && !dropdownRef.current.contains(e.target))
-            setShowMenu(false)
+            if (dropdownRef.current && !dropdownRef.current.contains(e.target))
+                setShowMenu(false)
         }
 
         document.addEventListener("click", closeMenu)
@@ -27,10 +27,13 @@ function BrowseDropdown () {
 
     return (
         <div className='browse-container'>
-            <Link onClick={toggleMenu} className="browse-links" to="/app/browse-books">Browse&nbsp;&#9660;</Link>
-            <div className={showMenu ? "dropdown" : "hidden"} ref={dropdownRef}>
-                <Link onClick={toggleMenu} to='/app/browse-books'>Browse Books</Link>
-                <Link onClick={toggleMenu} to='/app/browse-lists'>Browse Lists</Link>
+            <div className='browse-links'>
+                <Link className='browse-links' to='/app/browse-books'>Browse</Link>
+                <Link onClick={toggleMenu} className="browse-links" >&nbsp;&#9660;</Link>
+                <div className={showMenu ? "dropdown" : "hidden"} ref={dropdownRef}>
+                    <Link onClick={toggleMenu} to='/app/browse-books'>Browse Books</Link>
+                    <Link onClick={toggleMenu} to='/app/browse-lists'>Browse Lists</Link>
+                </div>
             </div>
         </div>
     )
