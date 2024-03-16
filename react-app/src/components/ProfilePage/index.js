9883c4f2b1a0306d0fd72edae663b51c0981e9d0
Emily <mocemmy@aol.com>
2024-03-16 16:41:15 -0400

    added profile picture with styling

--- react-app/src/components/ProfilePage/index.js
+++ react-app/src/components/ProfilePage/index.js
@@ -1,11 +1,16 @@
 import { useSelector } from "react-redux";
+import './ProfilePage.css'
 
 const ProfilePage = () => {
     //user information displayed:
     const user = useSelector(state => state.session.user)
+    console.log(user)
     return (
         <>
-            <h1>{user.first_name} {user.last_name}</h1>
+            <div className="profile-header">
+                <img src={user.profile_pic} />
+                <h1>{user.first_name} {user.last_name}</h1>
+            </div>
         </>
     )
 }
