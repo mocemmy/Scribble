f7784f1de84c44102217442e06261051db5e59ce
Emily <mocemmy@aol.com>
2024-03-30 22:20:34 -0400

    styled follow list

--- react-app/src/components/FollowComponents/UserDisplay.js
+++ react-app/src/components/FollowComponents/UserDisplay.js
@@ -1,11 +1,19 @@
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { thunkUnfollowUser } from "../../store/follow";
 
 const UserDisplay = ({ user }) => {
+    const dispatch = useDispatch();
+    const handleUnfollow = () => {
+        dispatch(thunkUnfollowUser(user.id))
+    }
     return (
         <div className="user-display">
-            <img className='profile-pic' src={user.profile_pic} alt='pic' />
-            <Link to={`/app/users/${user.id}`}>{user.first_name} {user.last_name} </Link>
-
+            <Link to={`/app/users/${user.id}`} className="profile-pic-name-container">
+                <img className='profile-pic' src={user.profile_pic} alt='pic' />
+                <p>{user.first_name} {user.last_name} </p>
+            </Link>
+            <button className="follow-button" onClick={handleUnfollow}>Unfollow</button>
         </div>
     )
 }
